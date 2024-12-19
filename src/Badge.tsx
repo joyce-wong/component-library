22252ccc010e2449af1fa1969c0bab2f8e455064
Joyce-Wong <joycekwo12@gmail.com>
2024-12-19 10:49:19 -0500

    deconstructed React imports

--- src/Badge.tsx
+++ src/Badge.tsx
@@ -1,15 +1,15 @@
-import React from "react"
+import { ReactNode, FC } from "react"
 import classnames from "classnames"
 
 interface BadgeProps {
-  children: React.ReactNode;
+  children: ReactNode;
   className?: string;
   shape: string;
   color?: string;
   [key: string]: any;
 }
 
-const Badge: React.FC<BadgeProps> = ({children, className, shape, color, ...rest}) => {
+const Badge: FC<BadgeProps> = ({children, className, shape, color, ...rest}) => {
   const shapeClass = shape && `button-${shape}`
   const colorClass = color && `button-${color}`
   const allClasses = classnames(shapeClass, colorClass, className)
